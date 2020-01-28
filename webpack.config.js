f5eb32a083d1bc59b19d14f0abcdfd017b370b3d
Albert <asgk912@gmail.com>
2020-01-27 18:38:46 -0800

    create configuration files for babel, jest, and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,6 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', "@babel/preset-react"],
-          }
         }
       }
     ]
