2318fdbfe2a0826f89243abc8b1361b5754c9f71
Albert <asgk912@gmail.com>
2020-01-28 18:08:34 -0800

    start working on summary module

--- client/comp/Summary.jsx
+++ client/comp/Summary.jsx
@@ -17,25 +17,25 @@ var Summary = ({property}) => {
   return (
     <div className="summary">
       <div className='line1'>
-        <span id="sumary_price">$ {property.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
+        <span id="summary_price">$ {property.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
         {vDivider}
-        <span id="summary_bed"> {property.bd} bd</span>
+        <span id="summary_bed">{property.bd} bd</span>
         {vDivider}
         <span id="summary_bath">{property.ba} ba</span>
         {vDivider}
-        <span id="summary_bath">{property.sqft.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} sqft </span>
+        <span id="summary_sqft">{property.sqft.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} sqft</span>
       </div>
-      <div className="line2" id="summary_address"> {property.address}</div>
+      <div className="line2" id="summary_address">{property.address}</div>
       <div className="line3">
         <div>
           <span id="summary_status">{property.status}</span>
           {vDivider}
-          <span id="summary_status">Zestimage: </span>
-          <span>$ {property.zestimate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
+          <span>Zestimate: </span>
+          <span id="summary_zestimate">$ {property.zestimate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
         </div>
       </div>
       <div className="line4">
-        <span id="estPayment">Est. payment: ${property.estPayment}/mo</span>
+        <span id="summary_estPayment">Est. payment: ${property.estPayment}/mo</span>
         <span style={{color: 'blue'}}>$</span>
         <span style={{color: 'blue'}}>Get pre-qualified</span>
       </div>
