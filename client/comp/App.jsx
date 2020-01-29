b6382c6f1e2106be1e588c6d965df5181f15efb3
Albert <asgk912@gmail.com>
2020-01-29 13:22:03 -0800

    save work before checkout to other branch

--- client/comp/App.jsx
+++ client/comp/App.jsx
@@ -1,6 +1,16 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
+import React from 'react';
+import sample from '../sample/sample.js'
+import Summary from './Summary.jsx';
+import NavigationBar from './NavigationBar.jsx';
 
-// ReactDOM.render(<div>App.jsx Testing</div>, document.getElementById('app'));
+var App = (props) =>{
+  return (
+    <div id="main_panel">
+      <Summary property={sample[0]}/>
+      <NavigationBar />
+      <div>Detail Panel</div>
+    </div>
+  )
+}
 
-// export default App
+export default App
---
+++ client/comp/NavigationBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+let NavigationBar = () => {
+  return (<div id='navbar'>Navigation Bar</div>);
+};
+
+export default NavigationBar;
