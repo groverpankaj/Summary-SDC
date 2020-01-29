6b27572d6acf2b9b1b080f11c0af6411f02488ae
Albert <asgk912@gmail.com>
2020-01-29 12:13:12 -0800

    connect server and database for query

--- database/retrieve.js
+++ database/retrieve.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 const House = require('./schema.js'); // model built for database
 
-const dbName = '7-xillow'       // database name 
+const dbName = '7-xillow'             // database name 
 
 let retrieve = (id, callback) =>{
   // connect to MongoDB
   mongoose.connect(`mongodb://localhost:27017/${dbName}`, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then((log) => { return House.find({id: id}, {_id:0, __v:0}) }) // find a house(document) with specific id number
+    .then((log) => { return House.findOne({id: id}, {_id:0, __v:0}) }) // find a house(document) with specific id number
     .then((data) => {
-      callback(null, data); // perform callback on data if retrieved
+      callback(null, data); // invoke callback with data as argument if retrieved
     })
     .catch((err) => {
-      calbacck(err, null); // perform callback on error if error occurs
+      calbacck(err, null); // invoke callback with with error as argument if error occurs
     });
+};
 
-}
-
-module.exports = retrieve
+module.exports = retrieve;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const port = 3002;
 const path = require('path');
 
+const retrieve = require('../database/retrieve.js'); // module for query a specific house in DB
+
 // set up server
 var app = express();
 app.set('port', port);
@@ -13,8 +15,21 @@ app.use(express.urlencoded({'extended': true}));
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 // response to request
-app.get('/api/data', (req, res) =>{
-  res.sendStatus(200);
+app.get('/api/data/:id', (req, res) =>{
+  var {id} = req.params;
+  retrieve(id, (err, data) =>{
+    if (err) { // if error occures, send status code of 500
+      console.log(err); 
+      res.send(500);
+    } else {
+      if(data){ // send data back to client if retrieved
+        res.jsonp(data);    
+      } else { // if query succeed but no data retrieve, send message to client about issue
+        res.send('no house with such id value');
+      }
+    }
+  });
+  
 })
 
 // start server
