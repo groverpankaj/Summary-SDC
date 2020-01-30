316a07d066c2fda1ac110f83e1da7601c9f1bc59
Albert <asgk912@gmail.com>
2020-01-30 15:31:56 -0800

    changed scrollTo to scrollIntoView

--- client/comp/App.jsx
+++ client/comp/App.jsx
@@ -8,40 +8,87 @@ import DetailPanel from './DetailPanel.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.scrollOnClick.bind(this);
+    this.state = {
+      showarrow: {left: false, right: true},
+      onview: {
+        'ov': true,
+        'ff': false,
+        'hv': false,
+        'pth': false,
+        'mc': false,
+        'rv': false,
+        'ns': false,
+        'ct': false,
+        'nh': false,
+        'hfy': false,
+        'sh': false
+      }
+    }
+
+    this.scrollOnClick = this.scrollOnClick.bind(this);
+    this.slideOnScroll = this.slideOnScroll.bind(this);
   }
 
   // function to scroll up and down through detail panel when navigation bar is clicked
   scrollOnClick(e) {
-    const clicked = e.target.className; // get classname of clicked one
-    const scrollTopValue = $(`div.${clicked}`)[0].offsetTop - $('#detailpanel')[0].offsetTop;
-    // compute how much needed to be scrolled down within the panel
+    const clicked = e.target.classList[2]; // get assgined classname of clicked one
+    
+    $(`div.${clicked}`)[0].scrollIntoView();
 
-    $('#detailpanel')[0].scrollTo({ left: 0, top: scrollTopValue, behavior: 'smooth' }); // scroll down the panel
+    /* implemntation with scrolTo function instead of scrollIntoView()
+    const scrollTopValue = $(`div.${clicked}`)[0].offsetTop - $('#detailpanel')[0].offsetTop; // compute how much needed to be scrolled down within the panel
+    $('#detailpanel')[0].scrollTo({ left: 0, top: scrollTopValue, behavior: 'smooth' });      // scroll down the panel
+    */
   }
 
   // functon to slide navigation bar when user scroll through the detail panel
   slideOnScroll(e) {
+    var htmlElement = event.target
+    htmlElement.scrollLeft;
+
     const scrollTopValue = e.target.scrollTop; // get how much is scrolled down from the top
     const offsetTopValue = scrollTopValue + $('#detailpanel')[0].offsetTop; // get pixel distance from the top of the page
 
-    const moduleID = ['ov', 'ff', 'hv', 'pth', 'mc', 'rv', 'ns', 'ct', 'nh', 'hfy', 'sh', 'dummy'];
+    const navNameInitial = Object.keys(this.state.onview);
+    
 
-    for (let i = 1; i < moduleID.length; i += 1) {
-      if (offsetTopValue < $(`div.${moduleID[i]}`)[0].offsetTop) { // find where panel is at
-        const scrollLeftValue = $(`span.${moduleID[i - 1]}`)[0].offsetLeft - $('#navbar')[0].offsetLeft;
+    for (let i = 1; i < navNameInitial.length + 1; i += 1) {
+      if(i === navNameInitial.length) { // handle case when the detail panel shows the last component 'similar homes'
+        $('span.sh')[0].scrollIntoView();
+        this.setState((state) => {
+          for (let key in state.onview) { // chnage rendering option
+              state.onview[key] = (key === 'sh') ? true : false;
+            }
+            return state;
+        });
+      } else { // handle the detail panel shows the other components
+        if (offsetTopValue < $(`div.${navNameInitial[i]}`)[0].offsetTop) { // find where panel is at
+          $(`span.${navNameInitial[i-1]}`)[0].scrollIntoView();
 
-        $('#navbar')[0].scrollTo({ left: scrollLeftValue, top: 0, behavior: 'smooth' });
-        break;
+          /* implemntation with scrolTo function instead of scrollIntoView()
+          const scrollLeftValue = $(`span.${navNameInitial[i - 1]}`)[0].offsetLeft - $('#navbar')[0].offsetLeft - 10;
+          $('#navbar')[0].scrollTo(scrollLeftValue, 0); // slide to navigation bar
+          */
+
+          this.setState((state) => {
+            for (let key in state.onview) { // chnage rendering option
+              state.onview[key] = (key === navNameInitial[i-1]) ? true : false;
+            }
+            return state;
+          })
+
+          break;
+        }
       }
     }
+    
   }
 
   render() {
     return (
       <div style={{ overflow: 'hidden' }}>
         <Summary property={sample[0]} />
-        <NavigationBar scrollOnClick={this.scrollOnClick} />
+        <NavigationBar onview={this.state.onview} scrollOnClick={this.scrollOnClick} />
         <DetailPanel slideOnScroll={this.slideOnScroll} />
       </div>
     );
