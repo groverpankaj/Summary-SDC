b95090171243ffaa522662bb37f3228eada9ed80
Albert <asgk912@gmail.com>
2020-01-30 11:25:17 -0800

    conform to airbnb lint-style guide

--- client/comp/NavigationBar.jsx
+++ client/comp/NavigationBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 /*
-  START: define styled component 
+  START: define styled component
 */
 
 // div to hide horizontal scroll bar of navigation bar
@@ -15,14 +15,14 @@ const ContainerToHideScroll = styled.div`
 `;
 
 // div that has scroll bar and contain contents' tiles
-const Styled_NavBar = styled.div`
+const StyledNavBar = styled.div`
   display: flex;
   overflow-x: auto;
   overflow-y: hidden;
   width: 100%;
   height: 60px;
   margin: 0px;
-`; 
+`;
 
 // span that holds contents' tile string
 const NavName = styled.span`
@@ -33,28 +33,29 @@ const NavName = styled.span`
 `;
 
 /*
-  END: define styled component 
+  END: define styled component
 */
+
 // <span className='ov' style={spanStyle} onClick={scrollOnClick}>Overview</span>
-let NavigationBar = ({scrollOnClick}) => {
-  var renderVal = (
-     <div style={{width: '500px'}}>
+const NavigationBar = ({ scrollOnClick }) => {
+  const renderVal = (
+    <div style={{ width: '500px' }}>
       <ContainerToHideScroll>
-        <div id='leftarrow' style={{position:'absolute', zIndex:1}}>&lt;</div>
-        <div id='rightarrow' style={{position:'absolute', left: '484px', zIndex:1}}>&gt;</div>
-        <Styled_NavBar id='navbar'>
-          <NavName className='ov' onClick={scrollOnClick}>Overview</NavName>
-          <NavName className='ff' onClick={scrollOnClick}>Facts and features</NavName>
-          <NavName className='hv' onClick={scrollOnClick}>Home value</NavName>
-          <NavName className='pth' onClick={scrollOnClick}>Price and tax history</NavName>
-          <NavName className='mc' onClick={scrollOnClick}>Monthly Cost</NavName>
-          <NavName className='rv' onClick={scrollOnClick}>Rental Value</NavName>
-          <NavName className='ns' onClick={scrollOnClick}>Nearby schools</NavName>
-          <NavName className='ct' onClick={scrollOnClick}>Commute time</NavName>
-          <NavName className='nh' onClick={scrollOnClick}>Neighborhood</NavName>
-          <NavName className='hfy' onClick={scrollOnClick}>Home for you</NavName>
-          <NavName className='sh' onClick={scrollOnClick}>Simlar Homes</NavName>
-        </Styled_NavBar>
+        <div id="leftarrow" style={{ position: 'absolute', zIndex: 1 }}>&lt;</div>
+        <div id="rightarrow" style={{ position: 'absolute', left: '484px', zIndex: 1 }}>&gt;</div>
+        <StyledNavBar id="navbar">
+          <NavName className="ov" onClick={scrollOnClick}>Overview</NavName>
+          <NavName className="ff" onClick={scrollOnClick}>Facts and features</NavName>
+          <NavName className="hv" onClick={scrollOnClick}>Home value</NavName>
+          <NavName className="pth" onClick={scrollOnClick}>Price and tax history</NavName>
+          <NavName className="mc" onClick={scrollOnClick}>Monthly Cost</NavName>
+          <NavName className="rv" onClick={scrollOnClick}>Rental Value</NavName>
+          <NavName className="ns" onClick={scrollOnClick}>Nearby schools</NavName>
+          <NavName className="ct" onClick={scrollOnClick}>Commute time</NavName>
+          <NavName className="nh" onClick={scrollOnClick}>Neighborhood</NavName>
+          <NavName className="hfy" onClick={scrollOnClick}>Home for you</NavName>
+          <NavName className="sh" onClick={scrollOnClick}>Simlar Homes</NavName>
+        </StyledNavBar>
       </ContainerToHideScroll>
     </div>
   );
