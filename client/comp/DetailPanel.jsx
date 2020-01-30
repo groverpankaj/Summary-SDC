6a96cbba0f2d764ae8d6e3213e163c7ba531d3e0
Albert <asgk912@gmail.com>
2020-01-30 11:49:53 -0800

    update eslint config and conform js/jsx files to the style

--- client/comp/DetailPanel.jsx
+++ client/comp/DetailPanel.jsx
@@ -1,152 +1,301 @@
 import React from 'react';
 
-let DetailPanel = ({slideOnScroll}) => {
+const DetailPanel = ({ slideOnScroll }) => (
 // this react element is merely to provide panel in which user can scroll up and down
 // the position within this panel will be referenced in other modules
-  return (
-    <div id="detailpanel" style={{height: '600px',overflowY: 'scroll'}} onScroll={slideOnScroll}>
-      <div className='ov'>Overview</div>
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-      <div className='ff'>Facts and features</div>
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-      <div className='hv'>Home value</div>
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-      <div className='pth'>Price and tax history</div>
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-      <div className='mc'>Monthly Cost</div>
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-      <div className='rv'>Rental Value</div>
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-      <div className='ns'>Nearby schools</div>
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-      <div className='ct'>Commute time</div>
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-      <div className='nh'>Neighborhood</div>
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-      <div className='hfy'>Home for you</div>
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-      <div className='sh'>Simlar Homes</div>
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
-        some detail<br />
+  <div id="detailpanel" style={{ height: '600px', overflowY: 'scroll' }} onScroll={slideOnScroll}>
+    <div className="ov">
+      <h5>Overview</h5>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
     </div>
-  );
-}
+    <div className="ff">
+      <h5>Facts and features</h5>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+    </div>
+    <div className="hv">
+      <h5>Home value</h5>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+    </div>
+    <div className="pth">
+      <h5>Price and tax history</h5>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+    </div>
+    <div className="mc">
+      <h5>Monthly Cost</h5>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+    </div>
+    <div className="rv">
+      <h5>Rental Value</h5>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+    </div>
+    <div className="ns">
+      <h5>Nearby schools</h5>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+    </div>
+    <div className="ct">
+      <h5>Commute time</h5>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+    </div>
+    <div className="nh">
+      <h5>Neighborhood</h5>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+    </div>
+    <div className="hfy">
+      <h5>Home for you</h5>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+    </div>
+    <div className="sh">
+      <h5>Simlar Homes</h5>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+        some detail
+      <br/>
+    </div>
+  </div>
+);
 
-export default DetailPanel
+export default DetailPanel;
