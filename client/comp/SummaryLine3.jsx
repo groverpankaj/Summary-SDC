7ba65f7ca07bb7c5863a89e397a9794c9ff608b6
Albert <asgk912@gmail.com>
2020-01-31 23:19:44 -0800

    change id and class name of html elements

--- client/comp/SummaryLine3.jsx
+++ client/comp/SummaryLine3.jsx
@@ -5,12 +5,12 @@ import { LineWrapper, Vdivider, SaleStatus, Zestimate} from './style.js'
 const SummaryLine3 = React.forwardRef(
   function SL3(props, ref) {
     return (
-      <LineWrapper id="summary_line3" fontsize='13px' ref={ref}>
-        <SaleStatus id="summary_status" status={this.props.saleStatus}><b>{this.props.saleStatus}</b></SaleStatus>
+      <LineWrapper fontsize='13px' ref={ref}>
+        <SaleStatus id="summary_status" status={props.saleStatus}><b>{props.saleStatus}</b></SaleStatus>
         <Vdivider/>
         <span id="summary_zestimate">
           <Zestimate>Zestimate<sup>®</sup>: </Zestimate>
-          ${this.props.zestimate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          ${props.zestimate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
         </span>
       </LineWrapper>
     );
