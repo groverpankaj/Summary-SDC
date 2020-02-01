131212a97df7db05f62e060e281f62fc0db4c5f9
Albert <asgk912@gmail.com>
2020-01-31 16:08:19 -0800

    add and update css styling

--- client/comp/Summary.jsx
+++ client/comp/Summary.jsx
@@ -1,93 +1,137 @@
 import React from 'react';
-
+import styled from 'styled-components';
 
 /*
   START: define styled-components
 */
 const SummaryWrapper = styled.div`
-  margin: 0px;
+  box-sizing: border-box;
+  width: 500px;
   padding: 10px 8px;
-  border-top: 1px solid #CCCCCC;
+  border-top: 1px solid #D1D1D1;
+  border-bottom: 1px solid #D1D1D1;
 `;
 
 const LineWrapper = styled.div`
-  margin: 0px;
-  padding: 0px;
+  padding: 2px 0px;
+  font-size: ${props => props.fontsize};
 `;
 
 const SpanPrice = styled.span`
-  padding-right: 10px;
-  font-size: 24px;
+  font-size: 28px;
+  font-weight: bold;
+  padding-right: 20px;
 `;
 
+
 const SpanBath = styled.span`
-  cursoer: help;
-  margin: 0px
+  cursor: help;
+  padding-bottom: 2px;
   border-bottom: 1px dashed #CCCCCC;
 `;
 
 const SpanStatus = styled.span`
   &::before{
+    display:inline-block;
+    margin-right: 8px;
+    border-radius: 50%;
+    background-color: ${props => props.status === 'For sale' ? '#ff594f' : 
+                                props.status === 'For rent' ? '#975cff' : '#ffd237'};
+    width: 10px;
+    height: 10px;
     content: "";
-    width: 5px;
-    height: 5px;
-    border-radius:50px;
-    ${props => (
-        (props.status === 'For sale' ? 'content:':'content');
-      )}
   }
+`;
+
+const SpanZestimate = styled.span`
+  cursor: pointer;
+  padding-bottom: 2px;
+  border-bottom: 1px dashed #CCCCCC;
+  font-weight: bold;
 `;
 
 const Vdivider = styled.span`
-  margin-left: 3px;
-  margin-right: 3px;
-  color: #d1d1d5;
-
-  &::before{
-    content: "|";
-  }
+  margin: 0px 9px;
+  border-right: 1px solid #d1d1d5;
 `;
-// const ButtonCA = styled.button`
-//   width: ${props => props.status? 100%}
-//   color: 006aff;
-// `
+
+const DollarIconWrapper = styled.span`
+  position: relative;
+  top: 7px;
+  padding: 0px 6px;
+  font-size: 24px;
+  color: #0074e4;
+`;
+
+const ButtonCA = styled.button`
+  margin-right: ${props => props.tourButton ? '8px' : '0px'};
+  border: 1px solid #aaaaaa;
+  border-radius: 3px;
+  width: ${props => props.tourButton ? '48%' : '100%'};
+  height: 40px;
+  background-color: #ffffff;
+  color: #006aff;
+  font-size: 15px;
+
+  &:hover{
+    border-color: #3f8fff;
+  }
+`
+const ButtonTT = styled.button`
+  margin-left: 8x;
+  border: 1px solid #006aff;
+  border-radius: 3px;
+  width: 48%;
+  height: 40px;
+  background-color: #006aff;
+  color: #ffffff;
+  font-size: 15px;
+
+  &:hover{
+    border: 1px solid #3f8fff;
+    background-color: #ffffff;
+    color: #006aff;
+  }
+`
+
 /*
   END: define styled-components
 */
 
 const Summary = ({ property }) => {
   const button = property.tour_button ? 
-                (<form><button type="button">Contact Agent</button></form>) :
-                (<form><button type="button">Contact Agent</button>
-                <button type="button">Take a tour</button></form>);
+                (<form>
+                  <ButtonCA type="button" tourButton={true}>Contact Agent</ButtonCA>
+                  <ButtonTT type="button">Take a tour</ButtonTT>
+                </form>) :
+                (<form><ButtonCA type="button">Contact Agent</ButtonCA></form>);
 
   return (
     <SummaryWrapper className="summary">
-      <div className='line1'>
+      <LineWrapper className='line1' fontsize='15px'>
         <SpanPrice id="summary_price">${property.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</SpanPrice>
+        <span id="summary_bed"><b>{property.bd}</b> bd</span>
         <Vdivider/>
-        <span id="summary_bed">{property.bd} bd</span>
+        <SpanBath id="summary_bath"><b>{property.ba}</b> ba</SpanBath>
         <Vdivider/>
-        <SpanBath id="summary_bath">{property.ba} ba</SpanBath>
-        <Vdivider/>
-        <span id="summary_sqft">{property.sqft.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} sqft</span>
-      </div>
-      <div className="line2" id="summary_address">{property.address}</div>
-      <div className="line3">
-        <div>
-          <span id="summary_status">{property.status}</span>
+        <span id="summary_sqft"><b>{property.sqft.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</b> sqft</span>
+      </LineWrapper>
+      <LineWrapper className="line2" id="summary_address" fontsize='14px' style={{fontWeight: 'bold'}}>{property.address}</LineWrapper>
+      <LineWrapper className="line3" fontsize='13px'>
+          <SpanStatus id="summary_status" status={property.status}><b>{property.status}</b></SpanStatus>
           <Vdivider/>
-          <span>Zestimate: </span>
-          <span id="summary_zestimate">$ {property.zestimate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
-        </div>
-      </div>
-      <SummaryWrapper className="line4">
-        <span id="summary_estPayment">Est. payment: ${property.estPayment}/mo</span>
-        <span style={{color: 'blue'}}>$</span>
-        <span style={{color: 'blue'}}>Get pre-qualified</span>
-      </div>
+          <span id="summary_zestimate">
+            <SpanZestimate>Zestimate<sup>®</sup>: </SpanZestimate>
+            ${property.zestimate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </span>
+      </LineWrapper>
+      <LineWrapper className="line4" fontsize='14px' style={{position: 'relative', top: '-10px'}}>
+        <span id="summary_estPayment"><b>Est. payment</b>: ${property.estPayment}/mo</span>
+        <DollarIconWrapper><i className="material-icons">monetization_on</i></DollarIconWrapper>
+        <span style={{color: '#0074e4'}}><b>Get pre-qualified</b></span>
+      </LineWrapper>
       {button}
-    </div>
+    </SummaryWrapper>
   );
 }
 
