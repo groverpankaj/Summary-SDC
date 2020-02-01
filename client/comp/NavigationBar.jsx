d3ff015d1056d428b1bc33eb5ac550b938ae05a2
Albert <asgk912@gmail.com>
2020-01-31 23:58:29 -0800

    refactor to accept react ref

--- client/comp/NavigationBar.jsx
+++ client/comp/NavigationBar.jsx
@@ -14,32 +14,32 @@ const navTitle = [ // array that holds string value of title, used to render tit
       'Similar Homes'
     ];
 
-const NavigationBar = ({ onview, showarrow, scrollOnClick, slideOnClick, showHideOnSlide }) => {
-  const renderVal = (
-    <Container>
-      <ContainerToHideScroll>
-        <StyledArrow id="leftarrow" 
-                      className="material-icons"
-                      style={{left: '2px', visibility: `${showarrow.left}`}}
-                      onClick={slideOnClick}>
-                      chevron_left
-        </StyledArrow>
-        <StyledArrow id="rightarrow"
-                      className="material-icons"
-                      style={{ left: '475px', visibility: `${showarrow.right}`}}
-                      onClick={slideOnClick}>
-                      chevron_right
-        </StyledArrow>
-        <StyledNavBar id="navbar" onScroll={showHideOnSlide}>
-          {Object.keys(onview).map((name, ind) => {
-            return <NavTitle key={ind} className={name} onClick={scrollOnClick} onview={onview[name]}>{navTitle[ind]}</NavTitle>
-          })}
-        </StyledNavBar>
-      </ContainerToHideScroll>
-    </Container>
-  );
-
-  return renderVal;
-};
+const NavigationBar = React.forwardRef(
+  function nv({ onview, showarrow, scrollOnClick, slideOnClick, showHideOnSlide }, ref) {
+    return (
+      <Container>
+        <ContainerToHideScroll>
+          <StyledArrow id="leftarrow" 
+                        className="material-icons"
+                        style={{left: '2px', visibility: `${showarrow.left}`}}
+                        onClick={slideOnClick}>
+                        chevron_left
+          </StyledArrow>
+          <StyledArrow id="rightarrow"
+                        className="material-icons"
+                        style={{ left: '475px', visibility: `${showarrow.right}`}}
+                        onClick={slideOnClick}>
+                        chevron_right
+          </StyledArrow>
+          <StyledNavBar id="navbar" onScroll={showHideOnSlide} ref={ref}>
+            {Object.keys(onview).map((name, ind) => {
+              return <NavTitle key={ind} className={name} onClick={scrollOnClick} onview={onview[name]}>{navTitle[ind]}</NavTitle>
+            })}
+          </StyledNavBar>
+        </ContainerToHideScroll>
+      </Container>
+    );
+  }
+);
 
 export default NavigationBar;
