9dfe267f59c4f392b27d9c5903e4d7bcbd8e8d9f
Albert <asgk912@gmail.com>
2020-02-02 16:24:49 -0800

    finish factoring out forwarding react ref

--- client/comp/ZestimatePopup.jsx
+++ client/comp/ZestimatePopup.jsx
@@ -2,31 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ZestimatePopup, LinkInPopup, PopupCloseButton, PopupIconLineI, PopupIconLineT } from './style.jsx';
 
-const Popup = React.forwardRef(
-  function zpu ({ popupLeft, popupWidth, hidePopupOnClick }, ref) {
-    return (
-      <ZestimatePopup id="zestimatePopup" left={popupLeft} width={popupWidth} >
-        <PopupCloseButton className="material-icons closeIcon" onClick={hidePopupOnClick}>close</PopupCloseButton>
+const Popup = ({ popupLeft, popupWidth, hidePopupOnClick }) => (
+  <ZestimatePopup id="zestimatePopup" left={popupLeft} width={popupWidth} >
+    <PopupCloseButton className="material-icons closeIcon" onClick={hidePopupOnClick}>close</PopupCloseButton>
 
-        <p>The Zestimate is Zillow&apos;s best estimate of this home&apos;s market value. It is not an appraisal and it should be used as a starting point. <LinkInPopup href="https://www.zillow.com/zestimate/">Learn more</LinkInPopup>.</p>
-        <p>If your facts are wrong, your Zestimate may be incorrect. <LinkInPopup href="#">Update them here</LinkInPopup>.</p>
-        <p>The Zestimate incorporates multiple data models and responds to factors like:</p>
+    <p>The Zestimate is Zillow&apos;s best estimate of this home&apos;s market value. It is not an appraisal and it should be used as a starting point. <LinkInPopup href="https://www.zillow.com/zestimate/">Learn more</LinkInPopup>.</p>
+    <p>If your facts are wrong, your Zestimate may be incorrect. <LinkInPopup href="#">Update them here</LinkInPopup>.</p>
+    <p>The Zestimate incorporates multiple data models and responds to factors like:</p>
 
-        <div className="zpu_iconline">
-            <PopupIconLineI className="material-icons">directions_walk</PopupIconLineI>
-            <PopupIconLineT>Neighborhood details</PopupIconLineT>
-            <PopupIconLineI className="material-icons-outlined">house</PopupIconLineI>
-            <PopupIconLineT>Home facts</PopupIconLineT>
-        </div>
-        <div className="zpu_iconline">
-            <PopupIconLineI className="material-icons">visibility</PopupIconLineI>
-            <PopupIconLineT>Popularity on Zillow</PopupIconLineT>
-            <PopupIconLineI className="material-icons-outlined">calendar_today</PopupIconLineI>
-            <PopupIconLineT>Listing price</PopupIconLineT>
-        </div>
-      </ZestimatePopup>
-    )
-  }
+    <div className="zpu_iconline">
+        <PopupIconLineI className="material-icons">directions_walk</PopupIconLineI>
+        <PopupIconLineT>Neighborhood details</PopupIconLineT>
+        <PopupIconLineI className="material-icons-outlined">house</PopupIconLineI>
+        <PopupIconLineT>Home facts</PopupIconLineT>
+    </div>
+    <div className="zpu_iconline">
+        <PopupIconLineI className="material-icons">visibility</PopupIconLineI>
+        <PopupIconLineT>Popularity on Zillow</PopupIconLineT>
+        <PopupIconLineI className="material-icons-outlined">calendar_today</PopupIconLineI>
+        <PopupIconLineT>Listing price</PopupIconLineT>
+    </div>
+  </ZestimatePopup>
 );
 
 Popup.propTypes = {
